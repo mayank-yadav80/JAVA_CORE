154d16eb11e287a68d52dd09e2dcae6d51f05a26
Mayank yadav <mayankyadav805735@gmail.com>
2025-09-16 12:39:51 +0530

    Add files via upload

---
+++ array/level1/VehicleManagementSystem.java
@@ -0,0 +1,44 @@
+interface Refuelable {
+    void refuel();
+}
+
+class Vehicle {
+    int maxSpeed;
+    String model;
+
+    Vehicle(int maxSpeed, String model) {
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+    }
+}
+
+class ElectricVehicle extends Vehicle {
+    ElectricVehicle(int maxSpeed, String model) {
+        super(maxSpeed, model);
+    }
+
+    void charge() {
+        System.out.println("Charging the electric vehicle");
+    }
+}
+
+class PetrolVehicle extends Vehicle implements Refuelable {
+    PetrolVehicle(int maxSpeed, String model) {
+        super(maxSpeed, model);
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Refueling the petrol vehicle");
+    }
+}
+
+public class VehicleManagementSystem {
+    public static void main(String[] args) {
+        ElectricVehicle ev = new ElectricVehicle(150, "Tesla");
+        PetrolVehicle pv = new PetrolVehicle(120, "Honda");
+
+        ev.charge();
+        pv.refuel();
+    }
+}
